30f6ba31c9d052024d31d797ede02ee83db717fb
Purple-Manue <purple.aformac@gmail.com>
2018-02-15 16:03:34 +0100

    fichier login.php et .gitignore

--- myFunction.php
+++ myFunction.php
@@ -1,9 +1,10 @@
 <?php
+include('login.php');
 
 	function mySqli(){
 		try{
 
-    		$bdd = new PDO('mysql:host=localhost;dbname=BDD_BLOG;charset=utf8','purple', 'Alchimie12');
+    		$bdd = new PDO('mysql:host=localhost;dbname=BDD_BLOG;charset=utf8', getUser(), getMdp());
     		return $bdd;
 		}
 		catch(Exception $e){
